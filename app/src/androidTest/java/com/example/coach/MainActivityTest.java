c8e904adfbf9613475927509c768882d4c7f34e2
jolupr <joanna.luciathe@gmail.com>
2024-03-15 13:33:48 +0100

    S2 : MVC, tests et code de la première interface graphique

---
+++ app/src/androidTest/java/com/example/coach/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.coach;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.example.coach.vue.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.os.SystemClock;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
+
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void scenario(){
+        onView(withId(R.id.btnCalc)).perform(click());
+        SystemClock.sleep(5000);
+    }
+}
